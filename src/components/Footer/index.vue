d64c95c0ea31a83681b5c9d5ca5c2888f265b5bf
Gregory <neotmiravsego@outlook.com>
2020-07-21 16:39:33 +0200

    add

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -10,11 +10,22 @@
   position: relative;
   height: 60px;
   background-color: black;
+  @media (max-width: $TableWidth) {
+    height: 90px;
+  }
 }
 .logo-footer {
   position: absolute;
   top: 50%;
-  right: 420px;
+  right: 20%;
   transform: translateY(-50%);
+  width: max-content;
+  @media (max-width: $TableWidth) {
+    display: flex;
+    align-items: flex-end;
+    left: 20px;
+    right: 0;
+    bottom: 0;
+  }
 }
 </style>
